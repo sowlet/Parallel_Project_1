3c05457e4975cfb6a2d7c4962782971f49cfb4c0
sowlet <134145311+sowlet@users.noreply.github.com>
2026-03-13 14:35:38 -0400

    Initial Push

---
+++ src/README.txt
@@ -0,0 +1,15 @@
+You can check your solution against mine by running the ProgLangSearch.jar file.
+As you do so, please keep in mind that our results for PageRank in particular 
+may differ since our algorithm is not deterministic. In order to run the 
+provided executable jar file, you must navigate to the same directory as the 
+file in your terminal and enter a command in the format listed below.
+
+java -jar ProgLangSearch.jar (equal|indegree|pagerank) (count|frequency|tf-idf) (arithmetic|geometric|harmonic)
+
+Example Commands:
+    java -jar ProgLangSearch.jar equal count arithmetic
+    java -jar ProgLangSearch.jar pagerank tf-idf geometric
+
+If you are unable to get all the functions working for your project, you may 
+write your report on the basis of the output of my solution, but you must note 
+that you have used my solution and will not receive full credit.
---
+++ src/main/scala/PageRank.scala
@@ -0,0 +1,24 @@
+import scala.util.Random
+import scala.collection.parallel.CollectionConverters._
+
+object PageRank {
+    /**
+     * @param pages A map of page.id to page for some number of WebPage objects
+     * @return      A map of page.id to a weight of 1.0 for those same WebPage objects
+     */
+    def equal(pages: Map[String, WebPage]): Map[String, Double] = {
+        Map() // TODO: remove this stub and implement this method
+    }
+
+    /**
+     * @param pages A map of page.id to page for some number of WebPage objects
+     * @return A map of page.id to a weight that is a simple count of the number of pages linking to that page
+     */
+    def indegree(pages: Map[String, WebPage]): Map[String, Double] = {
+        Map() // TODO: remove this stub and implement this method
+    }
+
+    def pagerank(pages: Map[String, WebPage]): Map[String, Double] = {
+        Map() // TODO: remove this stub and implement this method
+    }
+}
